0ee3d5cbd93f0b2d326e3e1b9abf166a174bd0ff
JR Rickerson <jrrickerson@gmail.com>
2017-07-06 16:05:27 -0400

    Read settings from environment / .env file.

--- podium/settings.py
+++ podium/settings.py
@@ -11,19 +11,24 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+dotenv_path = os.path.join(BASE_DIR, '.env')
+dotenv.load_dotenv(dotenv_path)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#q$k_9*v3cg5ml+-h4_^*yd1x%*6e@li@4e0$^6tl-0j&du^ef'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
 ALLOWED_HOSTS = []
 
@@ -80,6 +85,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+if 'DATABASE_URL' in os.environ.keys():
+    DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
