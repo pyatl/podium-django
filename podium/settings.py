4cef98a8640af3e4c4c3e3e98a150390c0ccf560
Jacques Cadet <Python3-77@users.noreply.github.com>
2017-07-12 10:15:47 -0400

    Update settings.py

--- podium/settings.py
+++ podium/settings.py
@@ -82,8 +82,7 @@ WSGI_APPLICATION = 'podium.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        ENGINE': 'django.db.backends.postgresql_psycopg2',
     }
 }
 
