29b3c05c5611e5de35a66316056e6d36cbde3f68
Python3-77 <mcadet347@gmail.com>
2017-07-12 11:29:26 -0400

    Update settings.py

--- podium/settings.py
+++ podium/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'podium.wsgi.application'
 
 DATABASES = {
     'default': {
-        ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
     }
 }
 
