ce35259909b0afeaa004f97e5c285d6ce3915e04
Luther <lutheroto@gmail.com>
2017-07-28 01:38:35 -0400

    Use '^$', not '', to detect empty URL.

--- podium/talks/urls.py
+++ podium/talks/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^submit/', views.submit_talk_view, name='talks-submit'),
     url(r'^sessions/', views.session_list_view, name='talks-sessions'),
-    url(r'', views.session_list_view, name='talks-index'),
+    url(r'^$', views.session_list_view, name='talks-index'),
 ]
