6739763dc36591b09ad945ccb1a18191f35564ba
Luther <lutheroto@gmail.com>
2017-08-01 10:12:16 -0400

    Get the view to show an exception

--- podium/talks/tests.py
+++ podium/talks/tests.py
@@ -1,13 +1,26 @@
 from datetime import date
 
-from django.test import TestCase
+from django.http import Http404
+from django.test import Client, TestCase
 
 from .models import Session
+from . import views
 
 
 class SessionTestCase(TestCase):
+
+    # Models
 
     def test_get_absolute_url(self):
         Session.objects.create(date=date(2020, 1, 1))
         self.assertEqual(
             Session.objects.get(id=1).get_absolute_url(), '/talks/sessions/1/')
+
+    # Views
+
+    def test_session_talk_list_view(self):
+        """Raise the right exception when the session does not exist."""
+        self.assertRaises(
+            Http404,
+            views.session_talk_list_view,
+            Client().get('/talks/sessions/1/'), 1)
--- podium/talks/views.py
+++ podium/talks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponseRedirect
-from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
 from .forms import TalkSubmissionForm
 from .models import Session
 
@@ -17,7 +17,8 @@ def submit_talk_view(request):
 
 
 def talk_detail_view(request, talk_id):
-    pass
+    raise Http404(
+        'This view is not implemented. Please tell the Podium maintainers to fix this.')
 
 
 def session_list_view(request):
@@ -27,5 +28,8 @@ def session_list_view(request):
 
 
 def session_talk_list_view(request, id):
-    raise Http404(
-        'This view is not implemented. Please tell the Podium maintainers to fix this.')
+    try:
+        session = Session.objects.get(id=id)
+    except Session.DoesNotExist:
+        raise Http404(f'Session {id} does not exist')
+    # We still have to render this.
