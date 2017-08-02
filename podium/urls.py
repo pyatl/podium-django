95038fa56680c5a120d2b7d18d2b0368c1c4a0eb
Luther <lutheroto@gmail.com>
2017-08-02 02:26:58 -0400

    Remove an unused import

--- podium/urls.py
+++ podium/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from .talks import views
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
