b3783a10ba3932d80c0a24855223011e2bd39d6b
JR Rickerson <jrrickerson@gmail.com>
2017-08-02 22:48:48 -0400

    Correct invalid string concatenations.

--- podium/settings.py
+++ podium/settings.py
@@ -105,23 +105,23 @@ if 'DATABASE_URL' in os.environ.keys():
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.\
-        UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.\
-        MinimumLengthValidator',
+        'django.contrib.auth.password_validation.'
+        'MinimumLengthValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.\
-        CommonPasswordValidator',
+        'django.contrib.auth.password_validation.'
+        'CommonPasswordValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.\
-         NumericPasswordValidator',
+        'django.contrib.auth.password_validation.'
+        'NumericPasswordValidator',
     },
 ]
 
