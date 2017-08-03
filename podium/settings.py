563ca225cb55b4b066c269840593f7ca31244b00
JR Rickerson <jrrickerson@gmail.com>
2017-08-02 22:55:49 -0400

    Remove an unnecessary template directory setting

--- podium/settings.py
+++ podium/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'templates/talks'), ],
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
