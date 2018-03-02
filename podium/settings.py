0d799b09c201af198830c08f826bc7294494d014
JR Rickerson <jrrickerson@gmail.com>
2018-03-01 20:44:16 -0500

    Settings: Fix ALLOWED_HOSTS for Heroku

--- podium/settings.py
+++ podium/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '.herokuapp.com',
+]
 
 
 # Application definition
