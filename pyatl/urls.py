7b37b637d202dcb7b7f8283e78dea0e92b10dea6
privera <privera@celerity.com>
2019-11-01 23:26:54 -0400

    fix import

--- pyatl/urls.py
+++ pyatl/urls.py
@@ -6,7 +6,7 @@ from pyatl.views import (
     LocationView,
     LocationsView,
     PageView,
-    EventDownloadCalendarInviteView,
+    EventInviteDownloadView,
 )
 
 urlpatterns = [
@@ -17,7 +17,7 @@ urlpatterns = [
 
     # event
     url(r'^event/(?P<date>[0-9]{4}-?[0-9]{2}-?[0-9]{2})/(?P<slug>[-\w\d]+)/(?P<pk>[0-9]+)/$', EventView.as_view(), name='event'),
-    url(r'^event/invite/(?P<pk>[0-9]+)/$', EventDownloadCalendarInviteView.as_view(), name='event-invite'),
+    url(r'^event/invite/(?P<pk>[0-9]+)/$', EventInviteDownloadView.as_view(), name='event-invite'),
     url(r'^events/$', EventsView.as_view(), name='events'),
 
     # location
